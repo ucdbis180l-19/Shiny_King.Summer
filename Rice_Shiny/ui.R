59b59dc54744e1b1039af640385e389f5f66b07f
summerYYY96 <summeryang96@gmail.com>
2019-05-09 21:11:25 +0000

    Edited

--- Rice_Shiny/ui.R
+++ Rice_Shiny/ui.R
@@ -16,16 +16,14 @@ read_csv("/home/ubuntu/Downloads/RiceSNPData/snpInfo.csv")
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Rice SNP Data"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+       radioButtons("trait",
+                    "Choose a trait to display:",
+                    c(''))
     ),
     
     # Show a plot of the generated distribution
